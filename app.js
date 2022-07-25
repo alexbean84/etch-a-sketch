4e3430926bb8b18383a897dda909aad87f43b2ea
alexbean84 <alexbean84@gmail.com>
2022-07-25 23:03:25 +0300

    Update makeRows function

--- app.js
+++ app.js
@@ -7,5 +7,11 @@ const grids = document.querySelector('.grid');
 function makeRows(rows, cols) {
     grids.style.setProperty('--grid-rows', rows);
     grids.style.setProperty('--grid-cols', cols);
-    
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement('div');
+        cell.innerText = (c + 1);
+        grids.appendChild(cell).className = 'grid-item';
+    }
 }
+
+makeRows(16, 16);
